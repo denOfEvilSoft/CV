7d4bd7689af43da7a1599819d31b6e3d216da044
denOfEvilSoft <88094842+denOfEvilSoft@users.noreply.github.com>
2021-07-28 23:36:24 +0900

    Add files via upload

---
+++ CV/CV/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿
+namespace CV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.Start = new System.Windows.Forms.Button();
+            this.Del = new System.Windows.Forms.Button();
+            this.stack_show = new System.Windows.Forms.Label();
+            this.full_show = new System.Windows.Forms.Label();
+            this.nanugi = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.Box = new System.Windows.Forms.TextBox();
+            this.Add = new System.Windows.Forms.Button();
+            this.Delay_Box = new System.Windows.Forms.TextBox();
+            this.Set_Delay = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.listBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.listBox1.Enabled = false;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(28, 16);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(145, 124);
+            this.listBox1.TabIndex = 0;
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(177, 28);
+            this.Start.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(69, 25);
+            this.Start.TabIndex = 1;
+            this.Start.Text = "시작";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // Del
+            // 
+            this.Del.Enabled = false;
+            this.Del.Location = new System.Drawing.Point(177, 57);
+            this.Del.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Del.Name = "Del";
+            this.Del.Size = new System.Drawing.Size(69, 25);
+            this.Del.TabIndex = 3;
+            this.Del.Text = "선택 삭제";
+            this.Del.UseVisualStyleBackColor = true;
+            this.Del.Click += new System.EventHandler(this.Del_Click);
+            // 
+            // stack_show
+            // 
+            this.stack_show.AutoSize = true;
+            this.stack_show.Location = new System.Drawing.Point(188, 14);
+            this.stack_show.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.stack_show.Name = "stack_show";
+            this.stack_show.Size = new System.Drawing.Size(11, 12);
+            this.stack_show.TabIndex = 4;
+            this.stack_show.Text = "0";
+            // 
+            // full_show
+            // 
+            this.full_show.AutoSize = true;
+            this.full_show.Location = new System.Drawing.Point(220, 14);
+            this.full_show.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.full_show.Name = "full_show";
+            this.full_show.Size = new System.Drawing.Size(17, 12);
+            this.full_show.TabIndex = 5;
+            this.full_show.Text = "10";
+            // 
+            // nanugi
+            // 
+            this.nanugi.AutoSize = true;
+            this.nanugi.Location = new System.Drawing.Point(205, 14);
+            this.nanugi.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.nanugi.Name = "nanugi";
+            this.nanugi.Size = new System.Drawing.Size(11, 12);
+            this.nanugi.TabIndex = 6;
+            this.nanugi.Text = "/";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("굴림", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(45, 337);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(201, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "문의 : denofevil.soft@gmail.com";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label1.Location = new System.Drawing.Point(2, 233);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(0, 2, 0, 0);
+            this.label1.Size = new System.Drawing.Size(236, 74);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "컨 + C로 등록 할 때 바로 등록이 안되고 이\r\n전에 복사했던 것들이 등록 된다면 지연시\r\n간올려라. 지금 지연시간 + 50 씩 하면 됨. \r\n" +
+    "잘 된다 싶으면 -10 씩 하면서 최적 값 찾으\r\n면 된다.\r\n기본 값 : 400 ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 17);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(11, 120);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "1\r\n2\r\n3\r\n4\r\n5\r\n6\r\n7\r\n8\r\n9\r\n0";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.AutoSize = false;
+            this.trackBar1.LargeChange = 2;
+            this.trackBar1.Location = new System.Drawing.Point(177, 87);
+            this.trackBar1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(69, 23);
+            this.trackBar1.TabIndex = 10;
+            this.trackBar1.Value = 10;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // Box
+            // 
+            this.Box.Location = new System.Drawing.Point(13, 143);
+            this.Box.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Box.Name = "Box";
+            this.Box.Size = new System.Drawing.Size(234, 21);
+            this.Box.TabIndex = 15;
+            // 
+            // Add
+            // 
+            this.Add.Location = new System.Drawing.Point(177, 113);
+            this.Add.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(69, 25);
+            this.Add.TabIndex = 16;
+            this.Add.Text = "수동 추가";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // Delay_Box
+            // 
+            this.Delay_Box.Font = new System.Drawing.Font("굴림", 9F);
+            this.Delay_Box.Location = new System.Drawing.Point(58, 175);
+            this.Delay_Box.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Delay_Box.Name = "Delay_Box";
+            this.Delay_Box.Size = new System.Drawing.Size(70, 21);
+            this.Delay_Box.TabIndex = 17;
+            // 
+            // Set_Delay
+            // 
+            this.Set_Delay.Location = new System.Drawing.Point(132, 175);
+            this.Set_Delay.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Set_Delay.Name = "Set_Delay";
+            this.Set_Delay.Size = new System.Drawing.Size(69, 21);
+            this.Set_Delay.TabIndex = 18;
+            this.Set_Delay.Text = "지연 시간";
+            this.Set_Delay.UseVisualStyleBackColor = true;
+            this.Set_Delay.Click += new System.EventHandler(this.Set_Delay_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(81, 205);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 12);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "지연 시간 : ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(150, 205);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(23, 12);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "400";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(252, 357);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Set_Delay);
+            this.Controls.Add(this.Delay_Box);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.Box);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.nanugi);
+            this.Controls.Add(this.full_show);
+            this.Controls.Add(this.stack_show);
+            this.Controls.Add(this.Del);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "ㅂ5ㄱ - BㅜE";
+            this.TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Button Del;
+        private System.Windows.Forms.Label stack_show;
+        private System.Windows.Forms.Label full_show;
+        private System.Windows.Forms.Label nanugi;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TextBox Box;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.TextBox Delay_Box;
+        private System.Windows.Forms.Button Set_Delay;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ CV/CV/Form1.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace CV
+{
+    public partial class Form1 : Form
+    {
+        CGlobalKeyboardHook hooker = new CGlobalKeyboardHook();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int stack = 0;
+        int delay_input = 400;
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            if (listBox1.Enabled == false)
+            {
+                listBox1.BackColor = Color.FromArgb(250, 250, 250);
+                listBox1.Enabled = true;
+                Del.Enabled = true;
+                Delay_Box.Enabled = false;
+                Set_Delay.Enabled = false;
+                Start.Text = "실행중...";
+                hooker.hook();
+                hooker.KeyDown += hooker_KeyDown;
+            }
+            else
+            {
+                listBox1.BackColor = Color.FromArgb(240, 240, 240);
+                listBox1.Enabled = false;
+                Del.Enabled = false;
+                Delay_Box.Enabled = true;
+                Set_Delay.Enabled = true;
+                Start.Text = "시작";
+                hooker.unhook();
+                hooker.KeyDown -= hooker_KeyDown;
+            }
+        }
+        private void hooker_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.C)
+            {
+                if(int.Parse(stack_show.Text) >= 10)
+                {
+                    MessageBox.Show("꽉 찼어.\n필요없는거 삭제 해");
+                    return;
+                }
+                Thread.Sleep(delay_input);
+                string temp = Clipboard.GetText();
+                listBox1.Items.Add(temp);
+                stack++;
+                stack_show.Text = stack.ToString();
+            }
+            string input = e.KeyCode.ToString();
+            
+            int temp22 = e.KeyValue;
+            switch (input)
+            {
+                case "D1":
+                    if (e.KeyCode == Keys.D1)
+                    {
+                        if (stack == 0)
+                        {
+                            MessageBox.Show(new Form { TopMost = true }, (stack + 1) +"번은 비어있어");
+
+                            SendKeys.Send("{BS}");
+                            return;
+                        } //
+                        Clipboard.SetText(listBox1.Items[0].ToString()); // 배열 이미 빼버렸음 필요 없으니까 이 부분을 인덱스접근으로 바꿔
+                        SendKeys.Send("^{v}");
+                        Thread.Sleep(delay_input);
+                        SendKeys.Send("{BS}");
+                    }
+                    break;
+                case "D2":
+                    if (e.KeyCode == Keys.D2)
+                    {
+                        if (stack < 2)
+                        {
+                            MessageBox.Show(new Form { TopMost = true }, (stack + 1) + "번은 비어있어");
+                            return;
+                        }
+                        Clipboard.SetText(listBox1.Items[1].ToString());
+                        SendKeys.Send("^{v}");
+                        Thread.Sleep(delay_input);
+                        SendKeys.Send("{BS}");
+                    }
+                    break;
+
+                case "D3":
+                    if (e.KeyCode == Keys.D3)
+                    {
+                        if (stack < 3)
+                        {
+                            MessageBox.Show(new Form { TopMost = true }, (stack + 1) + "번은 비어있어");
+                            return;
+                        }
+                        Clipboard.SetText(listBox1.Items[2].ToString());
+                        SendKeys.Send("^{v}");
+                        Thread.Sleep(delay_input);
+                        SendKeys.Send("{BS}");
+                    }
+                    break;
+
+                case "D4":
+                    if (e.KeyCode == Keys.D4)
+                    {
+                        if (stack < 4)
+                        {
+                            MessageBox.Show(new Form { TopMost = true }, (stack + 1) + "번은 비어있어");
+                            return;
+                        }
+                        Clipboard.SetText(listBox1.Items[3].ToString());
+                        SendKeys.Send("^{v}");
+                        Thread.Sleep(delay_input);
+                        SendKeys.Send("{BS}");
+                    }
+                    break;
+
+                case "D5":
+                    if (e.KeyCode == Keys.D5)
+                    {
+                        if (stack < 5)
+                        {
+                            MessageBox.Show(new Form { TopMost = true }, (stack + 1) + "번은 비어있어");
+                            return;
+                        }
+                        Clipboard.SetText(listBox1.Items[4].ToString());
+                        SendKeys.Send("^{v}");
+                        Thread.Sleep(delay_input);
+                        SendKeys.Send("{BS}");
+                    }
+                    break;
+
+                case "D6":
+                    if (e.KeyCode == Keys.D6)
+                    {
+                        if (stack < 6)
+                        {
+                            MessageBox.Show(new Form { TopMost = true }, (stack + 1) + "번은 비어있어");
+                            return;
+                        }
+                        Clipboard.SetText(listBox1.Items[5].ToString());
+                        SendKeys.Send("^{v}");
+                        Thread.Sleep(delay_input);
+                        SendKeys.Send("{BS}");
+                    }
+                    break;
+
+                case "D7":
+                    if (e.KeyCode == Keys.D7)
+                    {
+                        if (stack < 7)
+                        {
+                            MessageBox.Show(new Form { TopMost = true }, (stack + 1) + "번은 비어있어");
+                            return;
+                        }
+                        Clipboard.SetText(listBox1.Items[6].ToString());
+                        SendKeys.Send("^{v}");
+                        Thread.Sleep(delay_input);
+                        SendKeys.Send("{BS}");
+                    }
+                    break;
+
+                case "D8":
+                    if (e.KeyCode == Keys.D8)
+                    {
+                        if (stack < 8)
+                        {
+                            MessageBox.Show(new Form { TopMost = true }, (stack + 1) + "번은 비어있어");
+                            return;
+                        }
+                        Clipboard.SetText(listBox1.Items[7].ToString());
+                        SendKeys.Send("^{v}");
+                        Thread.Sleep(delay_input);
+                        SendKeys.Send("{BS}");
+                    }
+                    break;
+
+                case "D9":
+                    if (e.KeyCode == Keys.D9)
+                    {
+                        if (stack < 9)
+                        {
+                            MessageBox.Show(new Form { TopMost = true }, (stack + 1) + "번은 비어있어");
+                            return;
+                        }
+                        Clipboard.SetText(listBox1.Items[8].ToString());
+                        SendKeys.Send("^{v}");
+                        Thread.Sleep(delay_input);
+                        SendKeys.Send("{BS}");
+                    }
+                    break;
+
+                case "D0":
+                    if (e.KeyCode == Keys.D0)
+                    {
+                        if (stack < 10)
+                        {
+                            MessageBox.Show(new Form { TopMost = true }, (stack + 1) + "번은 비어있어");
+                            return;
+                        }
+                        Clipboard.SetText(listBox1.Items[9].ToString());
+                        SendKeys.Send("^{v}");
+                        Thread.Sleep(delay_input);
+                        SendKeys.Send("{BS}");
+                    }
+                    break;
+            }
+        }
+        private void Del_Click(object sender, EventArgs e)
+        {
+            string delete = "";
+            if(stack == 0)
+            {
+                MessageBox.Show("삭제할 텍스트가 없어");
+                return;
+            }
+            if ((listBox1.SelectedItem) != null)
+            {
+              delete = listBox1.SelectedItem.ToString();
+            }
+            else
+            {
+                MessageBox.Show("선택 안했어");
+                return;
+            }
+            listBox1.Items.Remove(delete); 
+             stack--;
+            stack_show.Text = stack.ToString();
+            Thread.Sleep(delay_input);
+        }
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            double opa = (trackBar1.Value + 1) * 0.1;
+            if(opa > 1)
+            {
+                opa = 1;
+            }
+            this.Opacity = opa;
+        }
+        private void Add_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(Box.Text.ToString());
+            Thread.Sleep(delay_input);
+            stack++;
+            Box.Text = "";
+        }
+        private void Set_Delay_Click(object sender, EventArgs e)
+        {
+            int re;
+            if (!int.TryParse(Delay_Box.Text, out re))
+            {
+                MessageBox.Show("자연수만 입력해");
+                return;
+            }
+            delay_input = re;
+            label5.Text = re.ToString();
+            Delay_Box.Text = "";
+        }
+    }
+}
